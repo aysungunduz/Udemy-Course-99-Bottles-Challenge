7ed01c45c09da8f747a13175a58ffbcfd727d9f0
aysun gunduz <113923715+aysungunduz@users.noreply.github.com>
2023-06-08 13:13:46 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let i = 99;
+while (i > 0) {
+    if (i > 1) {
+        console.log(`${i} bottles of beer on the wall, ${i} bottles of beer. 
+Take 1 down and pass it around, ${i - 1} bottles of beer on the wall.
+    
+    `);
+    }
+    else if (i = 1) {
+        console.log(`${i} bottle of beer on the wall, ${i} bottle of beer. 
+Take 1 down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.
+        `);
+    }
+
+    i--;
+}
